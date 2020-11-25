bf3c6683e90024e142133cb1235157cc8522cf7c
GreenDeer2001 <01DawidS@gmail.com>
2020-11-25 11:57:33 +0100

    url fixing

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-export const useHints =  (searchValue) => {
+export const useHints = (searchValue) => {
   const [hints, setHints] = useState([]);
 
   useEffect(() => {
@@ -10,19 +10,23 @@ export const useHints =  (searchValue) => {
     if (searchValue !== "" && searchValue) {
       axios
         .get(
-          `http://geodb-free-service.wirefreethought.com//v1/geo/cities?limit=5&offset=0&namePrefix=${searchValue}&sort=-population`,
-          { cancelToken: source.token }
+          `https://wft-geo-db.p.rapidapi.com/v1/geo/cities?limit=5&offset=0&namePrefix=${searchValue}&sort=-population`,
+          {
+            cancelToken: source.token,
+            headers: {
+              "x-rapidapi-key":
+                "e1ba07d177msh785cb3f0dfb3fc6p135021jsn145775e92efc",
+              "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
+            },
+          }
         )
         .then((res) => {
           setHints(res.data.data);
         })
-        .catch((e) => {});
+        .catch(() => {});
     }
     return () => source.cancel();
   }, [searchValue]);
-  
+
   return [hints, setHints];
 };
-
-
-
